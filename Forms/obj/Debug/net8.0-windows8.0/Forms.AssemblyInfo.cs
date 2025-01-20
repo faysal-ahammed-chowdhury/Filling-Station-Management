56dee91604661592c06c42e119372e50d428327f
Faysal Ahammed Chowdhury <faysalahammedchowdhury01@gmail.com>
2025-01-21 00:48:31 +0600

    Committing local changes temporarily

--- Forms/obj/Debug/net8.0-windows8.0/Forms.AssemblyInfo.cs
+++ Forms/obj/Debug/net8.0-windows8.0/Forms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Forms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fab0829fc22202e3ebe391578218bea947d0f443")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+814dfc63d9886a394801f5e62d73f207a182a614")]
 [assembly: System.Reflection.AssemblyProductAttribute("Forms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Forms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
